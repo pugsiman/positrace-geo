aab99680973e626ba9cdac74ed9befe9bc74d420
Aviv Levinsky <levinskyaviv@gmail.com>
2025-09-08 13:42:12 -0700

    add locations route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :locations, only: %w[create]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+      # NOTE: Based on the wording of the task, I'm assuming you wanted a lookup based on :identifier rather than :id
+      resources :locations, only: %w[show destroy], param: :identifier, constraints: { identifier: /[0-z.-]+/ }
+    end
+  end
 end
