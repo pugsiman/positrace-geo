1f895a50efc503cb124a043bc41944888528a432
Aviv Levinsky <levinskyaviv@gmail.com>
2025-09-08 16:59:37 -0700

    add locations controller

---
+++ app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::LocationsController < ApplicationController
+  respond_to :json
+
+  def create
+    location = Location.find_or_create!(identifier: locations_params.fetch(:identifier))
+    location.update_geolocation!
+    render json: location, status: 201
+  end
+
+  def show
+    location = Location.find_by!(identifier: locations_params.fetch(:identifier))
+    render json: location, status: 200
+  end
+
+  def destroy
+    location = Location.destroy_by(identifier: locations_params.fetch(:identifier))
+    render json: location, status: 200
+  end
+
+  private
+
+  def locations_params
+    params.permit(:identifier)
+  end
+end
