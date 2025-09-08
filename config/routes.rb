827e8a401c56f65539bbb18e427c4c973a673981
Aviv Levinsky <levinskyaviv@gmail.com>
2025-09-08 13:44:01 -0700

    default api routes to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :locations, only: %w[create]
 
