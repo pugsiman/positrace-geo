d19c86f34f53f933dae0461bdce60ead84493369
Aviv Levinsky <levinskyaviv@gmail.com>
2025-09-08 16:59:29 -0700

    update add_geolocaiton! name

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
   validates :identifier, uniqueness: true
 
-  def add_geolocation!(client: IpstackClient)
+  def update_geolocation!(client: IpstackClient)
     data = client.search(term: identifier)
     update!(lonlat: "POINT(#{data.fetch(:longitude)} #{data.fetch(:latitude)})", status: :updated)
     # NOTE: alternatively, in high scale I want to assign to async background job rather than a blocking sync operation:
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  describe '#add_geolocation!' do
+  describe '#update_geolocation!' do
     let(:location) { create(:location_with_url) }
 
     context 'with valid identifier' do
       let(:client_stub) { double('Client', search: { latitude: 37.419, longitude: 122.075 }) }
 
       it 'saves the matching geolocation data' do
-        expect { location.add_geolocation!(client: client_stub) }.to change { location.lonlat }
+        expect { location.update_geolocation!(client: client_stub) }.to change { location.lonlat }
           .and change { location.status }.from('pending').to('updated')
 
         expect(location.lonlat).to be_a RGeo::Geos::CAPIPointImpl # change if PostGIS adapter implementation changes
@@ -19,7 +19,7 @@ RSpec.describe Location, type: :model do
       let(:client_stub) { double('Client', search: {}) }
 
       it 'raises an error' do
-        expect { location.add_geolocation!(client: client_stub) }.to raise_error StandardError
+        expect { location.update_geolocation!(client: client_stub) }.to raise_error StandardError
       end
     end
   end
