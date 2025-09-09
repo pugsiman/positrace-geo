23d5bd5f088f0dfe7b4ee35bbec9dd34e6b7db99
Aviv Levinsky <levinskyaviv@gmail.com>
2025-09-08 20:33:35 -0700

    add more clarifying comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # NOTE: Based on the wording of the task, I'm assuming we want a lookup based on :identifier rather than :id
+      # A given IP address and URL may disattach, and a physical location association with an IP address may change.
+      # Using IP/URL as identifiers enables us to use a hybrid approach where we avoid accumulating stale data
       resources :locations, only: %w[show destroy create], param: :identifier, constraints: { identifier: /[0-z.-]+/ }
     end
   end
